c93d6d4fabcafc23517af1d7ba20bcefbbcdd90c
vpolier <111066480+vpolier@users.noreply.github.com>
2023-03-03 14:59:15 +1000

    update  script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 //select all required elements
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
-const exit_btn = info_box.querySelector(".button .quit");
-const continue_btn = info_box.querySelector(".button .restart");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const result_box = document.querySelector(".result_box");
 const option_list = document.querySelector(".option_list");
 const time_line = document.querySelector(".header .time_line");
-const timeText = document.querySelector(".timer .time_left_txt");
-const timeCount = document.querySelector(".timer .timer_sec");
+const time_Text = document.querySelector(".timer .time_left_txt");
+const time_Count = document.querySelector(".timer .timer_sec");
 
 start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo"); //shows info box
+}
+
+// i exitQuiz button clicked
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide info box
 }
